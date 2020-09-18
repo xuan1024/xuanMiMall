8f5e2a92edb9c62111ddb882251c5d991a02e95d
MacBook pro <macbookpro@MacBookdeMacBook-Pro-2.local>
2020-09-18 20:46:37 +0800

    项目路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,74 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'  
+import Router from 'vue-router'
+import Home from '../pages/home'
+import Index from '../pages/index'
+import Detail from '../pages/detail'
+import Product from '../pages/product'
+import Cart from '../pages/cart'
+import Order from '../pages/order'
+import OrderConfirm from '../pages/orderConfirm'
+import OrderList from '../pages/orderList'
+import OrderPay from '../pages/orderPay'
+import AliPay from '../pages/alipay'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = []
+export default new Router({
+  routes:[
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      children:[
+        {
+          path: '/index',
+          name: 'index',
+          component: Index,
+        }, {
+          path: '/product/:id',  
+          name: 'product',
+          component: Product,
+        }, {
+          path: '/detail/:id',
+          name: 'detail',
+          component: Detail,
+        }
+        
+      ]
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: Cart
+    },{
+      path: '/order',
+      name: 'order',
+      component: Order,
+      children:[
+        {
+          path: 'list',
+          name: 'order-list',
+          component: OrderList,
+        },
+        {
+          path: 'confirm',
+          name: 'order-confirm',
+          component: OrderConfirm,
+        },
+        {
+          path: 'pay',
+          name: 'order-pay',
+          component: OrderPay,
+        },
+        {
+          path: 'alipay',
+          name: 'alipay',
+          component: AliPay,
+        }
+      ]
+    }
+  ]
 
-const router = new VueRouter({
-  routes
-})
+});
 
-export default router
+
