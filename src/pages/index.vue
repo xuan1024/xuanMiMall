4e3631c3b8569e0819208416be9f76f911ab0586
MacBook pro <macbookpro@MacBookdeMacBook-Pro-2.local>
2020-09-22 01:22:56 +0800

    banner侧边栏菜单

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,11 +6,19 @@
           <ul class="menu-wrap">
             <li class="menu-item">
               <a href="javascript:;">手机 电话卡</a>
-              <div class="childre"></div>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub, j) in item" :key="j">
+                    <a :href="sub?'/#/product/'+sub.id:''">
+                      <img :src="sub?sub.img:'/imgs/item-box-1.png'" alt />
+                      {{sub?sub.name: '小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">电视 盒子</a>
-              <div class="childre"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">笔记本 平板</a>
@@ -45,7 +53,9 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="banner"></div>
+      <div class="banner">
+          
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -103,6 +113,35 @@ export default {
           id: "",
           img: "/imgs/slider/slide-5.jpg"
         }
+      ],
+      menuList: [
+        [
+          {
+            id: 30,
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9"
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版"
+          },
+          {
+            id: 32,
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 Pro"
+          },
+          {
+            id: 33,
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G专区"
+          }
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
       ]
     };
   }
@@ -126,20 +165,56 @@ export default {
           height: 50px;
           line-height: 50px;
           a {
-            display: block;
-            position: relative;
+              position: relative;
+            display: block;    
             font-size: 16px;
-            color: #fff;
+            color: #ffffff;
             padding-left: 30px;
-            &::after {
-              content: " ";
+            &:after {
+              content: ' ';
               position: absolute;
               right: 30px;
               top: 17.5px;
               @include bgImg(10px, 15px, "/imgs/icon-arrow.png");
             }
+        }
             &:hover {
               background-color: $colorA;
+              .children{
+                  display: block;
+                  
+              }
+            
+          }
+          .children {
+              display: none;
+            width: 962px;
+            height: 451px;
+            background-color: $colorG;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            border: 1px solid $colorH;
+            ul {
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li{
+                  height: 75px;
+                  line-height: 75px;
+                  flex:1;
+              }
+              a{
+                  color: $colorB;
+                  font-size: 14px;
+              }
+              img{
+                  width: 42px;
+                  height: 35px;
+                   vertical-align:middle;
+                  margin-right:15px;
+              }
+            
             }
           }
         }
